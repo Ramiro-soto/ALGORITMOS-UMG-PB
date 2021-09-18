a1138e8ac5f49407dd9677d4be48e697b77a6e80
“RAMIRO“ <jsoto12@miumg.edu.gt>
2021-09-18 08:10:00 -0600

    PRIMER COMMIT CLASE ALGORITMOS UMG PB 2021

---
+++ CALCULADORA.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+main()
+{
+  float n1, n2;
+  char op;
+  double r;
+  cout<<"Ingrese primer Nro: ";
+  cin>>n1;
+  cout<<"Ingrese operador (+,-,*,/,#): ";
+  cin>>op;
+  cout<<"Ingrese segundo Nro: ";
+  cin>>n2;
+  switch(op){case '+': r=n1+n2; break;
+             case '-': r=n1-n2; break;
+             case '*': r=n1*n2; break;
+             case '/': r=n1/n2; break;
+             case '#': r=pow(n1,n2); break;
+             default: cout<<"Operador no definido";
+             exit(1);
+             }
+  cout<<"El resultado es: "<<r;
+
+getch();
+}
---
+++ holamundo.cpp
@@ -0,0 +1,14 @@
+
+/* Clase de Algoritmos utilizando el Lenguaje de Programación C++ */
+
+#include<iostream>     // libreria
+
+using namespace std;   //espacio de nombres estandar y asi usar cout o cin
+
+main (){   //función donde empieza la ejecución,  puede contener int, void, entre otros
+	
+	cout <<"hola mundo";      //impresion o mostrar en pantalla 
+	
+	/* cout <<"hola mundo"; */        //codigo comentado
+}
+
